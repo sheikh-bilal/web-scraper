c8e01641605161dc99e3984fa3c3e47bcfa3c4c9
Bilal-Ahmed <reply2bilal.ahmed@gmail.com>
2025-04-30 19:44:57 +0500

    Split codebase into reusable components

--- web-scraper.js
+++ web-scraper.js
@@ -1,56 +1,13 @@
 const puppeteer = require("puppeteer");
 const ExcelJS = require("exceljs");
-const readline = require("readline");
+
+const {
+  askQuestion,
+  randomDelay,
+  autoScrollResults,
+} = require("./services/utils");
+
 let count = 0;
-// Random delay function to prevent getting banned
-function randomDelay(min = 1000, max = 2000) {
-  return new Promise((resolve) =>
-    setTimeout(resolve, Math.random() * (max - min) + min)
-  );
-}
-
-//fucntion for input
-function askQuestion(query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) =>
-    rl.question(query, (ans) => {
-      rl.close();
-      resolve(ans);
-    })
-  );
-}
-
-// Scroll the sidebar
-async function autoScrollResults(page) {
-  let previousHeight = 0;
-  let unchangedScrolls = 0;
-
-  while (unchangedScrolls < 3) {
-    const currentHeight = await page.evaluate(() => {
-      const scrollable = document.querySelector('div[role="feed"]');
-      if (scrollable) {
-        scrollable.scrollBy(0, 1000);
-        return scrollable.scrollHeight;
-      }
-      return 0;
-    });
-
-    if (currentHeight === previousHeight) {
-      unchangedScrolls++;
-    } else {
-      unchangedScrolls = 0;
-      previousHeight = currentHeight;
-    }
-
-    await randomDelay(2000, 3500);
-  }
-
-  console.log("Finished scrolling sidebar");
-}
 
 (async () => {
   const searchTerm = await askQuestion(
